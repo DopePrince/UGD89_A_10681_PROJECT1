18a5bb3450bca72de5d531294051c1340f51c71a
DopePrince <theys100@gmail.com>
2022-11-01 16:02:59 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ugd89_a_10681_project1/MainActivity.kt
@@ -0,0 +1,134 @@
+package com.example.ugd89_a_10681_project1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.annotation.SuppressLint
+import android.hardware.Camera
+import android.util.Log
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageButton
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.widget.Toast
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatDelegate
+
+class MainActivity : AppCompatActivity() {
+    private var mCamera: Camera? = null
+    private var mCameraView: CameraView? = null
+
+    lateinit var proximitySensor: Sensor
+    lateinit var sensorManager: SensorManager
+
+    var proximitySensorEventListener: SensorEventListener? = object : SensorEventListener {
+        override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+            return
+        }
+
+        override fun onSensorChanged(event: SensorEvent) {
+            if (event.sensor.type == Sensor.TYPE_PROXIMITY) {
+                if (event.values[0]==0f) {
+                    mCamera?.stopPreview()
+                    mCamera?.release()
+                    mCamera = Camera.open(Camera.CameraInfo.CAMERA_FACING_FRONT)
+                    mCameraView = CameraView(this@MainActivity, mCamera!!)
+                    val change_camera = findViewById<View>(R.id.FLCamera) as FrameLayout
+                    change_camera.addView(mCameraView)
+                } else {
+                    mCamera?.stopPreview()
+                    mCamera?.release()
+                    mCamera = Camera.open(Camera.CameraInfo.CAMERA_FACING_BACK)
+                    mCameraView = CameraView(this@MainActivity, mCamera!!)
+                    val change_camera = findViewById<View>(R.id.FLCamera) as FrameLayout
+                    change_camera.addView(mCameraView)
+                }
+            }
+
+//            if (event?.sensor?.type == Sensor.TYPE_ACCELEROMETER) {
+//                val sides = event.values[0]
+//                val upDown = event.values[1]
+//
+//                square.apply {
+//                    rotationX = upDown * 3f
+//                    rotationY = sides * 3f
+//                    rotation = -sides
+//                    translationX = sides * -10
+//                    translationY = upDown * 10
+//                }
+//
+//                val color = if (upDown.toInt() == 0 && sides.toInt() == 0) Color.GREEN else Color.RED
+//                square.setBackgroundColor(color)
+//
+//                square.text = "up/down ${upDown.toInt()}\nleft/right${sides.toInt()}"
+//
+//            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // PROXIMITY CODE
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY)
+
+        if (proximitySensor == null) {
+            Toast.makeText(this, "No proximity sensor found in device..", Toast.LENGTH_SHORT).show()
+            finish()
+        } else {
+            sensorManager.registerListener(
+                proximitySensorEventListener,
+                proximitySensor,
+                SensorManager.SENSOR_DELAY_NORMAL
+            )
+        }
+
+        // CAMERA CODE
+        try {
+            mCamera = Camera.open()
+        } catch (e: Exception) {
+            Log.d("Error", "Failed to get Camera" + e.message)
+        }
+        if (mCamera != null) {
+            mCameraView = CameraView(this, mCamera!!)
+            val camera_view = findViewById<View>(R.id.FLCamera) as FrameLayout
+            camera_view.addView(mCameraView)
+        }
+        @SuppressLint("MissingInflatedId", "LocalSuppress") val imageClose =
+            findViewById<View>(R.id.imgClose) as ImageButton
+        imageClose.setOnClickListener { view: View? -> System.exit(0) }
+
+        // ACCELEROMETER CODE
+
+        // NOTIFICATION CODE
+
+    }
+
+//    private fun setUpSensorStuff() {
+//        sensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
+//
+//        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
+//            sensorManager.registerListener(
+//                this,
+//                accelerometer,
+//                SensorManager.SENSOR_DELAY_FASTEST,
+//                SensorManager.SENSOR_DELAY_FASTEST
+//            )
+//        }
+//    }
+//
+//    override fun onDestroy() {
+//        sensorManager.unregisterListener(this)
+//        super.onDestroy()
+//    }
+
+}
